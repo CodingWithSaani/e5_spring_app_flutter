6d5ada1b0dedfad09600fcdc23ee65e085797240
codingwithsaani <codingwithsaani@gmail.com>
2023-05-30 11:56:02 +0500

    Lecture 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() => runApp(HeroExtended());
+
+class Hero extends StatelessWidget {
+  const Hero({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.blueGrey,
-    appBar: AppBar(
-      title: const Text("Hey App"),
-      backgroundColor: Colors.blueGrey,
-    ),
-    body: Image(image: NetworkImage("https://quizizz.com/media/resource/gs/quizizz-media/quizzes/c9711c76-aacc-4532-9a6e-2adf37315a2d"),),
-  )));
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.teal.shade500,
+          leading: Icon(
+            Icons.menu
+          ),
+          title: Text(
+              "Stateless Widget",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Text("Hey Hero !!",
+          style: TextStyle(
+            color: Colors.amber,
+            fontSize: 30.0
+          ),),
+        ),
+      ),
+    );
+  }
 }
+
+class HeroExtended extends StatefulWidget {
+  const HeroExtended({Key? key}) : super(key: key);
+
+  @override
+  State<HeroExtended> createState() => _HeroExtendedState();
+}
+
+class _HeroExtendedState extends State<HeroExtended> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.teal.shade500,
+          leading: Icon(
+              Icons.menu
+          ),
+          title: Text(
+            "Stateful Widget",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Text("Hey Hero Extended!!",
+            style: TextStyle(
+                color: Colors.amber,
+                fontSize: 30.0
+            ),),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
